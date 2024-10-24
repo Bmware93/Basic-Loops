ef22de8c3aaa0e7f89ce9f1d5c02d4add308e66e
Benia Morgan <bmorganware@gmail.com>
2024-10-24 19:10:36 -0400

    completed project

--- BasicLoops/Program.cs
+++ BasicLoops/Program.cs
@@ -1,54 +1,54 @@
-﻿////basic loops
+﻿//basic loops
 
-////do while loop exercise #1 
-//bool reprintMessage;
+//do while loop exercise #1 
+bool reprintMessage;
 
-//do
-//{
-//    Console.WriteLine("Hello World");
+do
+{
+    Console.WriteLine("Hello World");
 
-//    Console.WriteLine("Would you like to reprint this message? (y/n)");
-//    string userResponse = Console.ReadLine();
+    Console.WriteLine("Would you like to reprint this message? (y/n)");
+    string userResponse = Console.ReadLine();
 
-//    if (userResponse.ToLower() == "y")
-//    {
-//        reprintMessage = true;
-//    }
-//    else
-//    {
-//        reprintMessage = false;
-//    }
+    if (userResponse.ToLower() == "y")
+    {
+        reprintMessage = true;
+    }
+    else
+    {
+        reprintMessage = false;
+    }
 
-//} while (reprintMessage == true);
+} while (reprintMessage == true);
 
-////for loop exercise #2
-//Console.WriteLine("Enter a number:");
+//for loop exercise #2
+Console.WriteLine("Enter a number:");
 
-//bool continueProgram = true;
-//string userNumber = Console.ReadLine();
-//int userGuessAsInt = int.Parse(userNumber);
-//while(continueProgram == true)
-//{
-//    for (int number = userGuessAsInt; number >= 0; number--)
-//    {
-//        Console.Write(number + " ");
-//    }
-//    Console.WriteLine();
+bool continueProgram = true;
+string userNumber = Console.ReadLine();
+int userGuessAsInt = int.Parse(userNumber);
+while (continueProgram == true)
+{
+    for (int number = userGuessAsInt; number >= 0; number--)
+    {
+        Console.Write(number + " ");
+    }
+    Console.WriteLine();
 
-//    for (int number = 0; number <= userGuessAsInt; number++)
-//    {
-//        Console.Write(number + " ");
-//    }
+    for (int number = 0; number <= userGuessAsInt; number++)
+    {
+        Console.Write(number + " ");
+    }
 
-//    Console.WriteLine("Would you like to continue? (y/n)");
-//    string input = Console.ReadLine().ToLower();
+    Console.WriteLine("Would you like to continue? (y/n)");
+    string input = Console.ReadLine().ToLower();
 
-//    if(input != "y")
-//    {
-//        Console.WriteLine("Goodbye!");
-//        continueProgram = false;
-//    }
-//}
+    if (input != "y")
+    {
+        Console.WriteLine("Goodbye!");
+        continueProgram = false;
+    }
+}
 
 //Loop exercise #3
 string passcode = "13579";
